5b936803a3d5bd34f611cba3d0b2c706d7e202da
Jeffrey Rosselle <jeffrey.rosselle@digipolis.be>
2019-07-09 15:38:47 +0200

    Add project

---
+++ websocket-client/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebSocketClient
+{
+    class Program
+    {
+        private static readonly string Connection = "connectionstring";
+
+        static async Task Main(string[] args)
+        {
+            do
+            {
+                using (var socket = new ClientWebSocket())
+                    try
+                    {
+                        await socket.ConnectAsync(new Uri(Connection), CancellationToken.None);
+
+                        await Send(socket, "data");
+                        await Receive(socket);
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"ERROR - {ex.Message}");
+                    }
+            } while (true);
+        }
+
+        static async Task Send(ClientWebSocket socket, string data) =>
+            await socket.SendAsync(Encoding.UTF8.GetBytes(data), WebSocketMessageType.Text, true, CancellationToken.None);
+
+        static async Task Receive(ClientWebSocket socket)
+        {
+            var buffer = new ArraySegment<byte>(new byte[2048]);
+            do
+            {
+                WebSocketReceiveResult result;
+                using (var ms = new MemoryStream())
+                {
+                    do
+                    {
+                        result = await socket.ReceiveAsync(buffer, CancellationToken.None);
+                        ms.Write(buffer.Array, buffer.Offset, result.Count);
+                    } while (!result.EndOfMessage);
+
+                    if (result.MessageType == WebSocketMessageType.Close)
+                        break;
+
+                    ms.Seek(0, SeekOrigin.Begin);
+                    using (var reader = new StreamReader(ms, Encoding.UTF8))
+                        Console.WriteLine(await reader.ReadToEndAsync());
+                }
+            } while (true);
+        }
+    }
+}
