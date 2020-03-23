294356dcae2ed647b807f5e51a498f9c363f354f
Jeffrey Rosselle <me@jeffreyrosselle.me>
2020-03-24 00:35:03 +0100

    Add WebsocketService example

---
+++ websocket-client/WebsocketService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace websocket_client
+{
+    public class WebsocketService : BackgroundService
+    {
+        private static readonly string Connection = "connectionstring";
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+                using (var socket = new ClientWebSocket())
+                    try
+                    {
+                        await socket.ConnectAsync(new Uri(Connection), stoppingToken);
+
+                        await Send(socket, "data", stoppingToken);
+                        await Receive(socket, stoppingToken);
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"ERROR - {ex.Message}");
+                    }
+        }
+
+        private async Task Send(ClientWebSocket socket, string data, CancellationToken stoppingToken) =>
+            await socket.SendAsync(Encoding.UTF8.GetBytes(data), WebSocketMessageType.Text, true, stoppingToken);
+
+        private async Task Receive(ClientWebSocket socket, CancellationToken stoppingToken)
+        {
+            var buffer = new ArraySegment<byte>(new byte[2048]);
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                WebSocketReceiveResult result;
+                using (var ms = new MemoryStream())
+                {
+                    do
+                    {
+                        result = await socket.ReceiveAsync(buffer, stoppingToken);
+                        ms.Write(buffer.Array, buffer.Offset, result.Count);
+                    } while (!result.EndOfMessage);
+
+                    if (result.MessageType == WebSocketMessageType.Close)
+                        break;
+
+                    ms.Seek(0, SeekOrigin.Begin);
+                    using (var reader = new StreamReader(ms, Encoding.UTF8))
+                        Console.WriteLine(await reader.ReadToEndAsync());
+                }
+            };
+        }
+    }
+}
